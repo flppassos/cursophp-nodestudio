df68b7c4668dccfac04fd467cd9e568646ef1e15
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2020-12-09 17:58:01 -0300

    Criação da Aula 05

---
+++ aulas/05-fundamentosbasicos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 05 - Fundamentos Básicos</title>
+</head>
+<body>  
+    <?php
+        //Comentários na linha em php
+        /*Comentários
+        em mais de uma linhas*/
+
+        echo "Meu nome é Felipe. <br>";
+        echo "Minha idade é 27 anos";
+
+    ?>
+</body>
+</html>
