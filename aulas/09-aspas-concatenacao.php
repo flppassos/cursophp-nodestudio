317df9d6c48911765e7b8a901c781b1e6cd85954
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2020-12-11 11:35:59 -0300

    criado arquivo 09-aspas-concatenacao

---
+++ aulas/09-aspas-concatenacao.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = 'Felipe Passos';
+echo $nome;
+echo "<br>";
+//aspas simples
+echo 'Meu nome é '.$nome.' e tenho \'27\' anos'; //aspas simples são literais, não é interpretado, não le variável dentro das aspas. 
+echo "<br>";
+
+//aspas duplas
+echo "Meu nome é $nome";
+
+?>
