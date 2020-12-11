693aba3008eac7e7605fbdb7da3ea94ee772588f
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2020-12-11 10:12:04 -0300

    criado arquivo 07-variaveis-variaveis

---
+++ aulas/07-variaveis-variaveis.php
@@ -0,0 +1,20 @@
+<?php
+//variaveis variaveis
+$bebida = "refrigerante";
+$$bebida = "guaraná";
+
+echo $bebida;
+echo "<br>";
+echo $refrigerante;
+echo "<hr>";
+/////////////////////////////////////////////////////////////////////////////
+
+$destino = "cidade";
+$$destino = "Pirapora";
+
+echo $destino;
+echo "<br>";
+echo $cidade;
+echo "<hr>";
+
+?>
