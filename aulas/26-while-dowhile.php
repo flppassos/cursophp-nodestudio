f9f621baf8a6395ad8406e65c6a06f01f4148be5
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2020-12-18 18:22:32 -0300

    criado arquivo 26-while-dowhile

---
+++ aulas/26-while-dowhile.php
@@ -0,0 +1,15 @@
+<?php
+//while & do while
+$i = 1;
+$a = 1;
+
+while ($i <= 10){
+    echo "Valor de i: $i <br>";
+    $i++;
+}
+echo "<hr>";
+
+do {
+    echo "Valor de a: $a <br>";
+    $a++;
+} while ($a <= 10);
