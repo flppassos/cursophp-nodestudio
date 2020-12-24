74df33172ddc97a85c916f8a2f9097ebda9d2f49
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2020-12-24 15:50:53 -0300

    Update 30-criando-funcoes.php

--- aulas/30-criando-funcoes.php
+++ aulas/30-criando-funcoes.php
@@ -10,7 +10,13 @@ echo "<hr>";
 
 function somaNota($aluno, $n1, $n2, $n3){
     $soma = $n1 + $n2 + $n3;
-    echo "O Aluno $aluno, tirou a nota: $soma";
+    if ($soma >= 60){
+        echo "O Aluno $aluno, tirou a nota: $soma e foi aprovado.";
+    }
+    else{
+        echo "$aluno, você tirou a nota: $soma e foi Reprovado.";
+    }
+     
 }
 
 somaNota("Felipe", 25,17,21);
