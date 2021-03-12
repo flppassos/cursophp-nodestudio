675f588433a592f84c6c2c02cc671d43bee3e354
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2021-03-12 15:59:02 -0300

    password_hash

--- aulas/32-criptografia.php
+++ aulas/32-criptografia.php
@@ -9,3 +9,15 @@ echo "<hr>";
 
 echo "Md5: ". md5($senha)."<br>";
 echo "Sha1: ". sha1($senha);
+echo "<hr>";
+
+/*$senhaDb = '$2y$10$lYrAv8cRwn9vFac5UeVSX.pVKp/efo6hyo7WI3Fl0qsBU/B0PIedG';
+
+if (password_verify($senha, $senhaDb)) {
+    echo "Senha Válida";
+}else {
+    echo "Senha inválida";
+}*/
+
+$senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
+echo $senhaSegura;
