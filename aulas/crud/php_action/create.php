779ba93164dcd98c8641bf393e3dc4513b68d04c
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2021-03-16 10:34:26 -0300

    Ataque XSS

--- aulas/crud/php_action/create.php
+++ aulas/crud/php_action/create.php
@@ -3,12 +3,21 @@
 require_once 'connect.php';
 //Iniciar Sessão
 session_start();
+//Clear
+function clear($input) {
+    global $connect;
+    //sql
+    $var = mysqli_escape_string($connect, $input);
+    // xss
+    $var = htmlspecialchars($var);
+    return $var;
+}
 
 if (isset($_POST['btn-cadastrar'])) {
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
     //var_dump($_POST);
     $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', $idade)";
 
