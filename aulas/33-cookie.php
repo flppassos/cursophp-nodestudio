6328782ccdf53605831e08b6c13f4edd523239f4
Felipe Passos <48699056+flppassos@users.noreply.github.com>
2021-03-18 15:18:09 -0300

    Update 33-cookie.php

--- aulas/33-cookie.php
+++ aulas/33-cookie.php
@@ -1,3 +1,9 @@
 <?php
 //COOKIE
 setcookie('user', 'Felipe Passos', time()+3600);
+setcookie('email', 'felipef@minasligas.com.br', time()+3600); //para remover o cookie é só trocar o sinal do tempo para -
+setcookie('ultima_pesquisa', 'tenis adidas', time()+3600);
+
+echo "<pre>";
+var_dump($_COOKIE);
+echo "</pre>";
